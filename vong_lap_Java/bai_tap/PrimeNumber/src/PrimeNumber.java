868a9df424bc7d264221ad6f29b2cf490c7786b8
unknown <minhdhh91@gmail.com>
2024-09-09 16:30:34 +0700

    9/9/2024

---
+++ vong_lap_Java/bai_tap/PrimeNumber/src/PrimeNumber.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class PrimeNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of prime numbers to print: ");
+        int numbers = sc.nextInt();
+        int count = 0;
+        int N = 2;
+        while (count < numbers) {
+            if (isPrime(N)) {
+                System.out.println(N);
+                count++;
+            }
+            N++;
+        }
+    }
+
+    public static boolean isPrime(int n) {
+        if (n < 2) {
+            return false;
+        }
+        for (int i = 2; i <= n/2; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
---
+++ vong_lap_Java/bai_tap/ShapePrinter/src/ShapePrinter.java
@@ -0,0 +1,85 @@
+import java.util.Scanner;
+
+public class ShapePrinter {
+    public static void printRectangle(int width, int height) {
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+    public static void printSquareTriangleBottomLeft(int size) {
+        for (int i = 1; i <= size; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+    public static void printSquareTriangleTopLeft(int size) {
+        for (int i = size; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+    public static void printIsoscelesTriangle(int height) {
+        for (int i = 1; i <= height; i++) {
+            for (int j = i; j < height; j++) {
+                System.out.print("  ");
+            }
+            for (int k = 1; k <= (2 * i - 1); k++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int choice;
+
+        do {
+            System.out.println("\nMenu:");
+            System.out.println("1. Print the rectangle");
+            System.out.println("2. Print the square triangle (bottom-left)");
+            System.out.println("3. Print the square triangle (top-left)");
+            System.out.println("4. Print isosceles triangle");
+            System.out.println("5. Exit");
+            System.out.print("Enter your choice: ");
+            choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1:
+                    System.out.print("Enter width: ");
+                    int width = scanner.nextInt();
+                    System.out.print("Enter height: ");
+                    int height = scanner.nextInt();
+                    printRectangle(width, height);
+                    break;
+                case 2:
+                    System.out.print("Enter size of triangle: ");
+                    int size1 = scanner.nextInt();
+                    printSquareTriangleBottomLeft(size1);
+                    break;
+                case 3:
+                    System.out.print("Enter size of triangle: ");
+                    int size2 = scanner.nextInt();
+                    printSquareTriangleTopLeft(size2);
+                    break;
+                case 4:
+                    System.out.print("Enter height of isosceles triangle: ");
+                    int isoHeight = scanner.nextInt();
+                    printIsoscelesTriangle(isoHeight);
+                    break;
+                case 5:
+                    System.out.println("Exiting...");
+                    break;
+                default:
+                    System.out.println("Invalid choice! Please choose again.");
+            }
+        } while (choice != 5);
+    }
+}
